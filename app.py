3ed8765089d2e0ffaef1ed69fa4327fdcd1711cc
tuyenct0310-code <tuyenct0310@gmail.com>
2025-11-12 09:28:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def verify():
     token = request.args.get("hub.verify_token")
     challenge = request.args.get("hub.challenge")
     if token == VERIFY_TOKEN:
-        return challenge or "OK"
+        return str(challenge)
     return "Sai verify token", 403
 
 @app.route("/webhook", methods=["POST"])
@@ -65,4 +65,5 @@ def health():
     return jsonify(ok=True)
 
 if __name__ == "__main__":
+
     app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000)))
